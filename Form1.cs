bfe1f439843a28444180e525052dc7cb04ab3d23
DaimonxD <ulisesmartinmanias@gmail.com>
2023-11-02 10:48:17 -0300

    2/11 10:48

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace PEPITOMOSQUITOVECINA
 {
@@ -17,5 +18,32 @@ namespace PEPITOMOSQUITOVECINA
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Columns.Clear();
+
+            string sql = @"
+            SELECT P.ProductID, P.ProductName, C.Description
+            FROM Products P
+            INNER JOIN Categories C On P.CategoryID=C.CategoryID            
+            ORDER BY P.ProductName
+            ";
+
+            string cadena = "Data Source=PEPITO.db";
+
+
+            SQLiteDataAdapter adaptador = new SQLiteDataAdapter(sql, cadena);
+
+            DataTable tabla = new DataTable();
+
+            adaptador.Fill(tabla);
+
+            dataGridView1.DataSource = tabla;
+        }
     }
 }
